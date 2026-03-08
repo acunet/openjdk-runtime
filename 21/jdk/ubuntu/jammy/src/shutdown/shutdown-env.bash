346ab082b06ea489cff47790cd2b11f86e8a39a1
Acunet <mansurudien@gmail.com>
2026-03-08 13:36:36 +0700

    Update shutdown script paths for consistency in shutdown-env.bash and shutdown-wrapper.bash

--- 21/jdk/ubuntu/jammy/src/shutdown/shutdown-env.bash
+++ 21/jdk/ubuntu/jammy/src/shutdown/shutdown-env.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /setup-env.d/05-utils.bash
+source /opt/src/setup-env.d/05-utils.bash
 
 # If configured, output a thread dump and/or heap info on shutdown by wrapping the java process
 if is_true "${SHUTDOWN_LOGGING_THREAD_DUMP}" || is_true "${SHUTDOWN_LOGGING_HEAP_INFO}"; then
@@ -9,7 +9,7 @@ if is_true "${SHUTDOWN_LOGGING_THREAD_DUMP}" || is_true "${SHUTDOWN_LOGGING_HEAP
   random_sample=$(( RANDOM % 100 ))
   if (( random_sample <  SHUTDOWN_LOGGING_SAMPLE_THRESHOLD)); then
     echo "Shutdown logging threshold of ${SHUTDOWN_LOGGING_SAMPLE_THRESHOLD}% satisfied with sample ${random_sample}."
-    set -- /shutdown/shutdown-wrapper.bash "$@"
+    set -- /opt/src/shutdown/shutdown-wrapper.bash "$@"
   else
     echo "Shutdown logging threshold of ${SHUTDOWN_LOGGING_SAMPLE_THRESHOLD}% NOT satisfied with sample ${random_sample}."
   fi
